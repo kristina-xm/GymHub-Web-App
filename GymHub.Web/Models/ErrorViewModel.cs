736be736845658c57cf0c39b0ef2522f9a9ccfad
Kristina <kristina.hmn@gmail.com>
2023-07-19 23:27:41 +0300

    Initial files

---
+++ GymHub.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GymHub.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

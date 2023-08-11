50085d1eddcb4c87e4149e1fac44e4cbb4628ec2
Kristina <kristina.hmn@gmail.com>
2023-08-11 21:20:34 +0300

    Let Trainee cancel individual training

--- GymHub.Web/Controllers/TraineeController.cs
+++ GymHub.Web/Controllers/TraineeController.cs
@@ -7,10 +7,12 @@ namespace GymHub.Web.Controllers
     public class TraineeController : BaseController
     {
         private readonly ITraineeService traineeService;
+        private readonly IIndividualTrainingService trainingService;
 
-        public TraineeController(ITraineeService traineeService)
+        public TraineeController(ITraineeService traineeService, IIndividualTrainingService trainingService)
         {
             this.traineeService = traineeService;
+            this.trainingService = trainingService;
         }
 
 
@@ -27,6 +29,40 @@ namespace GymHub.Web.Controllers
             }
 
             return View("Dashboard", viewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CancelIndividualTraining(Guid TrainingId)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Account", "Trainee");
+            }
+
+            try
+            {
+                var training = await this.trainingService.GetTrainingById(TrainingId);
+
+                if (training == null)
+                {
+                    TempData[ErrorMessage] = "An error occured. Please try again";
+                    return RedirectToAction("Account", "Trainee");
+                }
+                else
+                {
+                    await this.trainingService.CancelTraining(training);
+                    TempData[SuccessMessage] = "You have successfully cancelled this training";
+                }
+
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError(string.Empty, "Unexpected error occured while saving your group activity registration. Please try again later");
+                return RedirectToAction("Account", "Trainee");
+            }
+
+            return RedirectToAction("Account", "Trainee");
         }
     }
 }
